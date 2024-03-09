fd14ffbd8db167baecc0569f23061ee59c78c7a8
dhoegl <firebird@hoeglund.com>
2024-03-09 13:34:22 -0800

    Re-installed 8.0 mysql version for compatibility; migrated Contact table via Prisma

---
+++ prisma/migrations/20240309205351_initial_migration/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Contact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `last` VARCHAR(100) NOT NULL,
+    `first` VARCHAR(100) NOT NULL,
+    `status` ENUM('ACTIVE', 'INACTIVE') NOT NULL DEFAULT 'INACTIVE',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
