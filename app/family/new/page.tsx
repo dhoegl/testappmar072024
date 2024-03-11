c4e92eae7b7e20068f9f40ae993ad680c1870afc
dhoegl <firebird@hoeglund.com>
2024-03-11 16:41:46 -0700

    Fixed favicon (cleared Edge cache) & setup form submission for New Family

--- app/family/new/page.tsx
+++ app/family/new/page.tsx
@@ -7,39 +7,45 @@ import React from 'react'
 
 interface FamilyForm {
     churchid: number;
-    description: string;
+    descriptor: string;
     last: string;
     first: string;
 }
 
 const NewFamilyMember = () => {
-    // const {register, control} = useForm<FamilyForm>();
-    // console.log(register('description'))
+    const { register, control, handleSubmit } = useForm<FamilyForm>();
+    console.log(register('descriptor'))
 
 
     return (
         <>
-            <div>
+            <form
+                onSubmit={handleSubmit((data) => console.log(data))}>
                 <div className="p-5 text-xl font-bold">
                     New Family Member
                 </div>
                 <div className='max-w-sm'>
                     <TextField.Root>
-                        <TextField.Input className='my-2' size="3" placeholder='Church ID' />
+                        <TextField.Input className='my-2' size="3" placeholder='Church ID' {...register('churchid')} />
                     </TextField.Root>
                     <TextField.Root className='space-y-3'>
-                        <TextField.Input className='my-2' size="3" placeholder='Last Name' />
+                        <TextField.Input className='my-2' size="3" placeholder='Last Name' {...register('last')}/>
                     </TextField.Root>
                     <TextField.Root className='space-y-3'>
-                        <TextField.Input className='my-2' size="3" placeholder='First Name' />
+                        <TextField.Input className='my-2' size="3" placeholder='First Name' {...register('first')} />
                     </TextField.Root>
+                    <Controller
+                        name="descriptor"
+                        control={control}
+                        render={({ field }) => <SimpleMDE className='space-y-3' placeholder='Description' {...field} />}
+                    />
                     {/* <TextArea className='space-y-3' placeholder='Description' /> */}
                     {/* <SimpleMDE className='space-y-3' placeholder='Description' /> */}
                 </div>
                 <div className='py-3'>
                     <Button>Submit New Family Member</Button>
                 </div>
-            </div>
+            </form>
         </>
     )
 }
