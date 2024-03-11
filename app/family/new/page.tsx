cedee7f9b143d10510f91caafd1cd3c54a554df6
dhoegl <firebird@hoeglund.com>
2024-03-11 14:47:04 -0700

    Lost the favicon - stopping until I find it

--- app/family/new/page.tsx
+++ app/family/new/page.tsx
@@ -1,10 +1,22 @@
 'use client'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import React from 'react'
 
+interface FamilyForm {
+    churchid: number;
+    description: string;
+    last: string;
+    first: string;
+}
+
 const NewFamilyMember = () => {
+    // const {register, control} = useForm<FamilyForm>();
+    // console.log(register('description'))
+
+
     return (
         <>
             <div>
@@ -22,7 +34,7 @@ const NewFamilyMember = () => {
                         <TextField.Input className='my-2' size="3" placeholder='First Name' />
                     </TextField.Root>
                     {/* <TextArea className='space-y-3' placeholder='Description' /> */}
-                    <SimpleMDE className='space-y-3' placeholder='Description' />
+                    {/* <SimpleMDE className='space-y-3' placeholder='Description' /> */}
                 </div>
                 <div className='py-3'>
                     <Button>Submit New Family Member</Button>
