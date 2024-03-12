7caf1147fb4b5da186a687468d960341fc9e183a
dhoegl <firebird@hoeglund.com>
2024-03-11 23:16:33 -0700

    Setup Home Page for Primary Entry if not authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+'use client'
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();  
+  router.push('/home');
   return (
     <div className="p-5 text-xl font-bold">Hello World!</div>
   );
